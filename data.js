ce369007a89eac5dad43d6b081a91d944f3303c5
d2shrepo <161230530+d2shrepo@users.noreply.github.com>
2025-04-19 19:28:24 +0300

    Update data.js

--- data.js
+++ data.js
@@ -18,7 +18,7 @@ const categories = [
     name: 'SSC',
     icon: 'SSC_SP.jpeg',
     channels: [
-      { name: 'SSC 1', url: 'https://example.com/ssc1.m3u8' },
+      { name: 'SSC 1', url: 'http://Uranus1.pro:80/live/392858619115/335685177211/712447.m3u8' },
 
       { name: 'SSC 2', url: 'https://example.com/ssc2.m3u8' }
 
