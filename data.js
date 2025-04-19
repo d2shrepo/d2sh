c7311ec057f2b2cef64db4d1fa906f06f2b86ff7
d2shrepo <161230530+d2shrepo@users.noreply.github.com>
2025-04-19 19:18:13 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,27 @@
+const categories = [
+  {
+    name: 'beIN',
+    icon: 'beIN_SP.png',
+    channels: [
+      { name: 'beIN Sports 1', url: 'http://Uranus1.pro:80/live/392858619115/335685177211/420133.m3u8' },
+
+      { name: 'beIN Sports 2', url: 'https://example.com/bein2.m3u8' },
+
+      { name: 'beIN Sports 3', url: 'https://example.com/bein2.m3u8' },
+
+      { name: 'beIN Sports 4', url: 'https://example.com/bein2.m3u8' }
+
+
+    ]
+  },
+  {
+    name: 'SSC',
+    icon: 'SSC_SP.jpeg',
+    channels: [
+      { name: 'SSC 1', url: 'https://example.com/ssc1.m3u8' },
+
+      { name: 'SSC 2', url: 'https://example.com/ssc2.m3u8' }
+
+    ]
+  }
+];
