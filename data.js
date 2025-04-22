c1d64fb0c8cf398d8491ff7a25f2ed130159f74b
d2shrepo <161230530+d2shrepo@users.noreply.github.com>
2025-04-22 21:40:01 +0300

    Update data.js

--- data.js
+++ data.js
@@ -3,24 +3,72 @@ const categories = [
     name: 'beIN',
     icon: 'beIN_SP.png',
     channels: [
-      { name: 'beIN Sports 1', url: 'http://Uranus1.pro:80/live/392858619115/335685177211/420133.m3u8' },
+      { name: 'beIN Sports 1', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/3221.m3u8' },
 
-      { name: 'beIN Sports 2', url: 'https://example.com/bein2.m3u8' },
 
-      { name: 'beIN Sports 3', url: 'https://example.com/bein2.m3u8' },
 
-      { name: 'beIN Sports 4', url: 'https://example.com/bein2.m3u8' }
+      { name: 'beIN Sports 2', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/3222.m3u8' },
+
+
+
+      { name: 'beIN Sports 3', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/3224.m3u8' },
+
+
+
+      { name: 'beIN Sports 4', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/3225.m3u8' },
+
+
+      { name: 'beIN Sports 5', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/3226.m3u8' },
+
+
+
+      { name: 'beIN Sports 6', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/3227.m3u8' },
+
+
+
+      { name: 'beIN Sports 7', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/3228.m3u8' },
+
+
+
+      { name: 'beIN Sports 8', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/3230.m3u8' },
+
+
+      { name: 'beIN Sports 9', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/4033.m3u8' }
 
 
     ]
   },
+
+
+
+
+
+
   {
     name: 'SSC',
     icon: 'SSC_SP.jpeg',
     channels: [
-      { name: 'SSC 1', url: 'http://Uranus1.pro:80/live/392858619115/335685177211/712447.m3u8' },
+      { name: 'SSC 1', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/4119.m3u8' },
 
-      { name: 'SSC 2', url: 'https://example.com/ssc2.m3u8' }
+
+      { name: 'SSC 2', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/3834.m3u8' },
+
+      
+      { name: 'SSC 3', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/3835.m3u8' },
+
+      { name: 'SSC 4', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/3836.m3u8' },
+
+
+      { name: 'SSC 5', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/3837.m3u8' },
+
+
+      { name: 'SSC Extra1', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/4128.m3u8'},
+
+
+      { name: 'SSC Extra2', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/4130.m3u8'},
+
+
+      { name: 'SSC Extra3', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/4234.m3u8'}
 
     ]
   }
