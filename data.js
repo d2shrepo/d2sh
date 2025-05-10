188c73b9484ad4f9f2bc1e1e2439745a1638fd23
d2shrepo <161230530+d2shrepo@users.noreply.github.com>
2025-05-10 20:14:27 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,88 @@
+const categories = [
+  {
+    name: 'beIN',
+    icon: 'beIN_SP.png',
+    channels: [
+      { name: 'beIN Sports 1', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/3221.m3u8' },
+      { name: 'beIN Sports 2', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/3222.m3u8' },
+      { name: 'beIN Sports 3', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/3224.m3u8' },
+      { name: 'beIN Sports 4', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/3225.m3u8' },
+      { name: 'beIN Sports 5', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/3226.m3u8' },
+      { name: 'beIN Sports 6', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/3227.m3u8' },
+      { name: 'beIN Sports 7', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/3228.m3u8' },
+      { name: 'beIN Sports 8', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/3230.m3u8' },
+      { name: 'beIN Sports 9', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/4033.m3u8' }
+    ]
+  },
+
+  {
+    name: 'SSC',
+    icon: 'SSC_SP.jpeg',
+    channels: [
+      { name: 'SSC 1', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/3833.m3u8' },
+      { name: 'SSC 2', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/3834.m3u8' },
+      { name: 'SSC 3', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/3835.m3u8' },
+      { name: 'SSC 4', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/3836.m3u8' },
+      { name: 'SSC 5', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/3837.m3u8' },
+      { name: 'SSC Extra1', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/3838.m3u8' },
+      { name: 'SSC Extra2', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/4130.m3u8' },
+      { name: 'SSC Extra3', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/4234.m3u8' }
+    ]
+  },
+
+  {
+    name: 'AD Premium',
+    icon: 'AD_Premuime.jpeg', 
+    channels: [
+      { name: 'AD Premium 1', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/4040.m3u8' },
+
+
+      { name: 'AD Premium 2', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/4041.m3u8' }
+
+
+
+    ]
+  },
+
+/*  {
+    name: 'AD Sports',
+    icon: 'ad_sp.png',          
+    channels: [
+      { name: 'AD Sports 1', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/3301.m3u8' },
+      { name: 'AD Sports 2', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/3302.m3u8' },
+      { name: 'AD Sports 3', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/3303.m3u8' },
+      { name: 'AD Sports 4', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/3304.m3u8' },
+      { name: 'AD Sports Extra', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/3305.m3u8' }
+    ]
+  }*/
+
+  {
+    name: 'KSA Sports',
+    icon: 'yellow.jpeg',          
+    channels: [
+      { name: 'KSA Sports 1', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/2053.m3u8' },
+
+      { name: 'KSA Sports 2', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/2054.m3u8' },
+
+      { name: 'KSA Sports 3', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/2055.m3u8' }
+    ]
+  },
+
+
+
+   {
+    name: 'ALKASS',
+    icon: 'Al_kass.jpeg',          
+    channels: [
+      { name: 'Alkass 1', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/1461.m3u8' },
+      { name: 'Alkass 2', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/1462.m3u8' },
+      { name: 'Alkass 3', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/1463.m3u8' },
+      { name: 'Alkass 4', url: 'http://ugeen.live:8080/live/Ugeen_VIP5avoqu/r40Ppu/1464.m3u8' }
+
+    ]
+   }
+
+
+
+
+];
